e7cb5ddd36536d1f618a4e549381d2bec0314c75
shweyioo <shweyioosyo1234@gmail.com>
2021-07-23 13:11:39 +0630

    23.7.2021

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll('.panel');
+// console.log(panels);
+
+panels.forEach((panel) => {
+    // console.log(panel);
+
+    panel.addEventListener('mouseenter', () => {
+        removeactiveprevclass();
+        panel.classList.add('active');
+    });
+})
+
+
+function removeactiveprevclass(){
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    })
+}
